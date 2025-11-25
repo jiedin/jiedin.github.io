6dfccccd5e075dd189a3f6dbbf93b645683c6624
jiedin <jayding@bu.edu>
2025-11-25 04:51:16 +0000

    Deploying to gh-pages from @ jiedin/jiedin.github.io@874a7ef5a9ad2f85cdfc85483c01dfa541baded5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,14 +72,6 @@ ninja.data = [{
       },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
           section: "News",},{
         id: 'social-cv',
         title: 'CV',
